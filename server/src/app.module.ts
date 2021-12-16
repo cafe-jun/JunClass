f216fef4e7f751c3d53fdac4a01504e48367713e
Cafejun <tb25271@gmail.com>
2021-12-16 23:17:24 +0900

    [Server] auth,class Module & Controller & Service generate

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,8 +7,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 // import { DatabaseConfig } from './config/databaseConfig';
 import { UserModule } from './user/user.module';
 import { typeORMConfig } from './config/typeorm.config';
+import { ClassModule } from './class/class.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, ClassModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
