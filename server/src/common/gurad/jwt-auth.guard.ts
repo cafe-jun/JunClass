5d7a950dbe335d23e5ec638c5965f242ffc2652c
Cafejun <tb25271@gmail.com>
2022-01-18 06:42:00 +0900

    [JWT]moring Test

--- server/src/common/gurad/jwt-auth.guard.ts
+++ server/src/common/gurad/jwt-auth.guard.ts
@@ -1,16 +1,21 @@
 import { AuthGuard } from '@nestjs/passport';
-import { CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  BadRequestException,
+  HttpCode
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 export class JwtAuthGuard implements CanActivate {
-  canActcanActivate(
+  canActivate(
     context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true
+    const request = context.switchToHttp().getRequest();
+    if (!request.headers.authorization)
+      throw new BadRequestException('authorization Not Found.');
+    const authorization: string = request.headers.authorization;
+    const token = authorization.slice(authorization.lastIndexOf('Bearer '));
+    console.log(authorization.indexOf('Bearer', -1));
+    return true;
   }
-  // handleRequest(err, user, info) {
-  //   console.log(err);
-  //   console.log(`user :${user}`);
-  //   if (err || !user) throw err || new UnauthorizedException();
-  //   return user;
-  // }
 }
