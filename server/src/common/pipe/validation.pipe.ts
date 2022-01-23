9aefba1b73528e52bd527ab6623a961652513dd1
Cafejun <tb25271@gmail.com>
2022-01-23 19:18:54 +0900

    [Custom Pipe] Pipe Custom

--- server/src/common/pipe/validation.pipe.ts
+++ server/src/common/pipe/validation.pipe.ts
@@ -10,15 +10,13 @@ import { validate } from 'class-validator';
 @Injectable()
 export class CustomValidationPipe implements PipeTransform<any> {
   async transform(value: any, { metatype }: ArgumentMetadata) {
-    console.log(value);
     if (!metatype || !this.validateMetaType(metatype)) return value;
     const object = plainToClass(metatype, value);
     const errors = await validate(object);
-    console.log(errors);
     if (errors.length > 0) {
+      console.log(errors);
       throw new BadRequestException('Validation Failed');
     }
-    console.log(value);
     return value;
   }
 
