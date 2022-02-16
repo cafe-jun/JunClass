e77c8d4886caedd0e6b3120d12f275f35d761911
Cafejun <tb25271@gmail.com>
2022-02-16 22:23:58 +0900

    [Write] Blog Write

---
+++ frontend/src/components/write/Editor.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Responsive from '../common/Responsive';
+import styled from 'styled-components';
+
+const EditorBlock = styled(Responsive)`
+  /* 페이지 위 아래 여백 지정 */
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+const TitleInput = styled.input`
+  font-size: 3rem;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[4]};
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+const QuillWrapper = styled.div`
+  /* 최소 크기 지정 및 padding 제거 */
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+  .ql-editor.ql-blank::before {
+    left: 0px;
+  }
+`;
+
+const Editor = () => {
+  return <div></div>;
+};
+
+export default Editor;
