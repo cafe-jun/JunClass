00316a6b2a84f185cc5a29e19146fabbdcac420e
Cafejun <tb25271@gmail.com>
2022-02-23 21:49:57 +0900

    nestJS 인터페이스를 활용하여 함수 재활용

--- frontend/src/pages/SignInForm.jsx
+++ frontend/src/pages/SignInForm.jsx
@@ -48,7 +48,8 @@ const SigInForm = ({ history }) => {
       try {
         localStorage.setItem('user', JSON.stringify(user));
       } catch (err) {
-        console.log('localStorage Test is test not working this Test');
+        console.log(err);
+        console.log('localStorage is test not working this Test');
       }
     }
   }, [history, user]);
