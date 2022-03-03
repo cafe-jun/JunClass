18c12af324b499acbc12e0f3761c58f23721ecc0
Cafejun <tb25271@gmail.com>
2022-03-03 21:44:10 +0900

    [Role]Base

--- frontend/src/pages/SignUpForm.jsx
+++ frontend/src/pages/SignUpForm.jsx
@@ -6,6 +6,7 @@ import { check } from '../modules/user';
 import { withRouter } from 'react-router-dom';
 
 const SignUpForm = ({ history }) => {
+  const [loading, setLoading] = useState();
   const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
